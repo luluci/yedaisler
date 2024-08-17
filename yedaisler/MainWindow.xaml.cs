12d1157dc76fd25a081a10b757d21d11f4b56a9a
luluci <lunacied@gmail.com>
2024-08-17 23:57:21 +0900

    構造見直し前コミット

--- yedaisler/MainWindow.xaml.cs
+++ yedaisler/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,9 +21,56 @@ namespace yedaisler
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isLocationChanged = false;
+
+        Config.Config config = new Config.Config();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //var vm = new MainWindowViewModel(this);
+            //this.DataContext = vm;
+
+            //this.Width = 150;
+            //this.Height = 100;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Init Config
+            var config_vm = config.DataContext as Config.ConfigViewModel;
+            await config_vm.InitAsync();
+
+            //config.ShowDialog();
+            var vm = this.DataContext as MainWindowViewModel;
+            vm.Init(config);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //マウスボタン押下状態でなければ何もしない  
+            if (e.ButtonState != MouseButtonState.Pressed) return;
+
+            this.DragMove();
+            //this.menu.IsOpen = false;
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!isLocationChanged)
+            {
+                this.menu.PlacementTarget = this;
+                this.menu.Placement = PlacementMode.Top;
+                this.menu.IsOpen = true;
+            }
+            isLocationChanged = false;
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            isLocationChanged = true;
+            this.menu.IsOpen = false;
         }
     }
 }
