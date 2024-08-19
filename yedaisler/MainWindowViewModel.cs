5c10755c4107c47fbbdcf9d9e8d374ab59e9d895
luluci <lunacied@gmail.com>
2024-08-19 12:30:39 +0900

    SubMenuItem背景色調整

---
+++ yedaisler/MainWindowViewModel.cs
@@ -0,0 +1,126 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using yedaisler.Utility;
+
+namespace yedaisler
+{
+    internal class MainWindowViewModel : BindableBase, IDisposable
+    {
+
+        public ReactivePropertySlim<double> Width { get; set; }
+        public ReactivePropertySlim<double> Height { get; set; }
+
+        public ReactivePropertySlim<double> SnapDistH { get; set; }
+        public ReactivePropertySlim<double> SnapDistV { get; set; }
+
+        public ReactiveCollection<ToDo.Item> ToDos { get; set; }
+
+        public ReactiveCommand AppExitCommand { get; set; }
+
+        // 色設定
+        public ReactivePropertySlim<SolidColorBrush> BrushBackground { get; private set; }
+        public ReactivePropertySlim<SolidColorBrush> BrushBaseFont { get; private set; }
+
+        // Config関連
+        Config.Config config;
+        Config.ConfigViewModel config_vm;
+
+        public MainWindowViewModel() {
+
+            SnapDistH = new ReactivePropertySlim<double>(1);
+            SnapDistH.AddTo(Disposables);
+            SnapDistV = new ReactivePropertySlim<double>(1);
+            SnapDistV.AddTo(Disposables);
+
+            Width = new ReactivePropertySlim<double>(50);
+            Width.Subscribe(x =>
+            {
+                SnapDistH.Value = x / 2;
+            })
+            .AddTo(Disposables);
+            Height = new ReactivePropertySlim<double>(30);
+            Height.Subscribe(x =>
+            {
+                SnapDistV.Value = x / 2;
+            })
+            .AddTo(Disposables);
+
+            AppExitCommand = new ReactiveCommand();
+            AppExitCommand.Subscribe(() =>
+            {
+                // App終了
+                System.Windows.Application.Current.Shutdown((int)System.Windows.ShutdownMode.OnExplicitShutdown);
+            });
+
+            // color
+            var bkcolor = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0xA0, 0x20, 0x20, 0x40));
+            var fontcolor = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF));
+            // Color
+            BrushBackground = new ReactivePropertySlim<SolidColorBrush>(bkcolor);
+            BrushBackground.AddTo(Disposables);
+            BrushBaseFont = new ReactivePropertySlim<SolidColorBrush>(fontcolor);
+            BrushBaseFont.AddTo(Disposables);
+
+            //
+            ToDos = new ReactiveCollection<ToDo.Item>();
+            ToDos.AddTo(Disposables);
+        }
+
+        public void Init(Config.Config config_)
+        {
+            // Configへの参照を記憶
+            config = config_;
+            config_vm = config.DataContext as Config.ConfigViewModel;
+
+            // Config.ToDoを制御用データに変換
+            foreach (var c_todo in config_vm.ToDos)
+            {
+                var todo = new ToDo.Item(c_todo);
+                todo.Init();
+                ToDos.Add(todo);
+            }
+        }
+
+        #region IDisposable Support
+        private CompositeDisposable Disposables { get; } = new CompositeDisposable();
+        private bool disposedValue = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    this.Disposables.Dispose();
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~MainWindowViewModel()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            //GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
