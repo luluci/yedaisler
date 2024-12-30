96131845bae73964c081567a496277314f938874
luluci <lunacied@gmail.com>
2024-12-30 13:49:03 +0900

    ColorPicker実装

--- yedaisler/Utility/ColorPickerViewModel.cs
+++ yedaisler/Utility/ColorPickerViewModel.cs
@@ -6,33 +6,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace yedaisler.Utility
 {
     internal class ColorPickerViewModel : BindableBase, IDisposable
     {
-
         public ReactivePropertySlim<int> Alpha { get; set; }
         public ReactivePropertySlim<int> Red {  get; set; }
         public ReactivePropertySlim<int> Blue { get; set; }
         public ReactivePropertySlim<int> Green { get; set; }
 
+        // private WriteableBitmap SVbitmap;
+        public ReactivePropertySlim<SolidColorBrush> Brush { get; set; }
+
         public ColorPickerViewModel()
         {
-            // http://kagenyan.blog69.fc2.com/blog-entry-139.html
-            // https://gogowaten.hatenablog.com/entry/2023/04/20/164232
+            Brush = new ReactivePropertySlim<SolidColorBrush>(new SolidColorBrush(System.Windows.Media.Color.FromArgb(255,255,255,255)));
 
-
-            Alpha = new ReactivePropertySlim<int>(255);
-            Alpha.AddTo(Disposables);
-            Red = new ReactivePropertySlim<int>(255);
-            Red.AddTo(Disposables);
-            Blue = new ReactivePropertySlim<int>(255);
-            Blue.AddTo(Disposables);
-            Green = new ReactivePropertySlim<int>(255);
-            Green.AddTo(Disposables);
+            Alpha = new ReactivePropertySlim<int>(255, ReactivePropertyMode.DistinctUntilChanged);
+            Alpha.Subscribe(x => {
+                UpdateBrush();
+            })
+            .AddTo(Disposables);
+            Red = new ReactivePropertySlim<int>(255, ReactivePropertyMode.DistinctUntilChanged);
+            Red.Subscribe(x => {
+                UpdateBrush();
+            })
+            .AddTo(Disposables);
+            Blue = new ReactivePropertySlim<int>(255, ReactivePropertyMode.DistinctUntilChanged);
+            Blue.Subscribe(x => {
+                UpdateBrush();
+            })
+            .AddTo(Disposables);
+            Green = new ReactivePropertySlim<int>(255, ReactivePropertyMode.DistinctUntilChanged);
+            Green.Subscribe(x => {
+                UpdateBrush();
+            })
+            .AddTo(Disposables);
         }
 
+        public void UpdateBrush()
+        {
+            Brush.Value = new SolidColorBrush(System.Windows.Media.Color.FromArgb((byte)Alpha.Value, (byte)Red.Value, (byte)Green.Value, (byte)Blue.Value));
+        }
 
 
 
